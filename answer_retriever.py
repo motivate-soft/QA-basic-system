786ac3769dd1304313f22c461fedaf676923d586
robert-upwork <robyop.eyeco@gmail.com>
2021-09-05 07:59:44 +0700

    Fix

--- answer_retriever.py
+++ answer_retriever.py
@@ -4,13 +4,17 @@ from transformers import DistilBertTokenizer, DistilBertForQuestionAnswering
 class AnswerRetriever:
 
     def getAnswer(self, question, questionContext):
+        # distilBertTokenizer = DistilBertTokenizer.from_pretrained('./model/base', return_token_type_ids=True)
+        # distilBertForQuestionAnswering = DistilBertForQuestionAnswering.from_pretrained(
+        #     './model/distilbert-base-uncased-distilled-squad', from_tf=False)
+        
         distilBertTokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased', return_token_type_ids=True)
         distilBertForQuestionAnswering = DistilBertForQuestionAnswering.from_pretrained(
             'distilbert-base-uncased-distilled-squad')
 
         encodings = distilBertTokenizer.encode_plus(question, questionContext)
 
-        inputIds, attentionMask = encodings["input_ids"], encodings["attention_mask"]
+        inputIds, attentionMask = encodings["input_ids"], encodings["token_type_ids"]
 
         scoresStart, scoresEnd = distilBertForQuestionAnswering(torch.tensor([inputIds]),
                                                                 attention_mask=torch.tensor([attentionMask]))
